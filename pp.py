1b6875c9aad90e580c44de262a16402d1aae2245
Your Name <you@example.com>
2023-11-14 19:16:40 +0600

    f

--- pp.py
+++ pp.py
@@ -1,14 +1,20 @@
-def remove_duplicates(nums):
-    if not nums:
-        return 0
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Creating a dummy node for reference
+        dummynode= ListNode(0)
+        current=dummynode
+        carry=0
 
-    j = 1
-    for i in range(1, len(nums)):
-        if nums[i] != nums[i-1]:
-            nums[j] = nums[i]
-            j += 1
+        while l1!= None or l2 != None or carry !=0:
+            # Checking if a node exists in the list
+            l1value = l1.val if l1 else 0
+            l2value = l2.val if l2 else 0
+            sum= l1value+l2value+carry
+            carry = sum //10 # Adding quotient value to carry
+            current.next=  ListNode(sum%10) # Adding remainder value to the node
+            # Updating list node pointers
+            current= current.next
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
 
-    return j
-nums = [1, 1, 2, 3]
-result_length = remove_duplicates(nums)
-print(result_length)
+        return dummynode.next 
